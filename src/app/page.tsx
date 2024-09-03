859cd106c2e9c528e3b5824c09bd35da540c777a
Balveer Singh <bs902423@gmail.com>
2024-09-03 14:59:52 +0530

    testing tmdb api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import TrendingMoviesPage from "./components/movies";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-slate-400">
       <h1 className="text-9xl font-black underline">Streamly</h1>
-      <h2 className="text-7xl font bold ">⬇</h2>
-      <h2 className="text-7xl font bold ">Your Go to destination for movies wiki</h2>
+      <TrendingMoviesPage />
     </main>
   );
 }
